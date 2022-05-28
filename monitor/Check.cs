55cbb65b038f7c3c28fac9221f9c87facd8e97ad
Alex Vahanyan <alexvahanyan@gmail.com>
2022-05-28 21:34:20 +0400

    Add project files.

---
+++ monitor/Check.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace monitor
+{
+    class Check
+    {
+        private int _lifespan;
+        private string _name;
+        public Check(string name, int lifespan)
+        {
+            _name = name;
+            _lifespan = lifespan;
+        }
+        public void CheckAndKill()
+        {
+            Process[] processes = Process.GetProcessesByName(_name);
+            foreach(Process process in processes)
+            {
+                Console.WriteLine(process.Id + " " + process.MainWindowTitle + " " + process.StartTime.ToString());
+                DateTime startTime = process.StartTime;
+                TimeSpan delta = DateTime.Now.Subtract(startTime);
+                if (delta.Minutes >= _lifespan)
+                {
+                    process.Kill();
+                    Console.WriteLine("Process killed");
+                }
+            }
+        }
+        public void Monitor(int freq)
+        {
+            while(true)
+            {
+                CheckAndKill();
+                Thread.Sleep(1000 * 60 * freq);
+            }
+        }
+    }
+}
---
+++ monitor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace monitor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the name of the process you want to monitor: ");
+            string name = Console.ReadLine();
+            Console.Write("Please enter the name of the lifespan in minutes: ");
+            int lifespan = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter the name of the frequency in minutes: ");
+            int freq = Convert.ToInt32(Console.ReadLine());
+
+            Thread checkThread = new Thread(() =>
+            {
+                Check check = new Check(name, lifespan);
+                check.Monitor(freq);
+            });
+            checkThread.Start();
+            
+            while (Console.ReadKey().KeyChar!='q')
+            {
+                continue;
+            }
+            checkThread.Abort();
+        }
+    }
+}
